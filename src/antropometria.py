84225686288f9766028bf51745b29e1631692327
ralphribeiro <ralph_ribeiro@msn.com>
2021-01-23 14:38:28 -0300

    antr

---
+++ src/antropometria.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+""" 
+"""
+
+@dataclass
+class Medidas:
+    estatura_1 = 177.5
+    altura_poplítea_sentado_2 = 46
+    altura_superior_coxa_assento_3 = 58.5 - altura_poplítea_sentado_2
+    altura_cotovelo_assento_4 = 64.5 - altura_poplítea_sentado_2
+    altura_lombar_sentado_5 = 71 - altura_poplítea_sentado_2
+    comprimento_nádega_polpídea_sentado_6 = 48
+    comprimento_nádega_joelho_sentado_7 = 60.4
+    largura_quadril_sentado_8 = 36.5
+
+
+
+
+
+
+
+
+
