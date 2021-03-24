946c56796c9b6946f1bd2c3a0e72f062d1ae62f0
Ralph Ribeiro <ralph_ribeiro@msn.com>
2021-03-24 20:26:49 -0300

    brincando de plotar

--- src/experimento_aranhacam.py
+++ src/experimento_aranhacam.py
@@ -1,3 +1,4 @@
+from array import array
 from dataclasses import dataclass, field
 from math import sqrt
 from random import randrange
@@ -28,10 +29,10 @@ class Linha:
         self.tamanho = sqrt((delta_x)**2 + (delta_y)**2 + (delta_z)**2)
 
 
-A = Ponto(-5, -5, 5)
-B = Ponto(-5, 5, 5)
-C = Ponto(5, 5, 5)
-D = Ponto(5, -5, 5)
+A = Ponto(-50, -50, 0)
+B = Ponto(-50, 50, 0)
+C = Ponto(50, 50, 0)
+D = Ponto(50, -50, 0)
 
 pontos_fixos = {'A': A, 'B': B, 'C': C, 'D': D}
 
@@ -67,9 +68,14 @@ def plota_normais(*args):
     ax.set_xlabel('X', fontsize='xx-small')
     ax.set_ylabel('Y', fontsize='xx-small')
     ax.set_zlabel('Z', fontsize='xx-small')
-    # ax.
+
+    minimo = -50
+    maximo = 50
+
     ax.legend(fontsize='xx-small')
-    ax.set_zlim3d([-5, 5])
+    ax.set_xlim3d([minimo, maximo])
+    ax.set_ylim3d([minimo, maximo])
+    ax.set_zlim3d([minimo, maximo])
 
 
 def curva_parametrica():
@@ -84,6 +90,47 @@ def curva_parametrica():
         yield Ponto(x[i], y[i], z[i])
 
 
+def obtem_ponto_pela_equacao_parametrica(
+    ponto: array, vetor: array, t=1
+) -> array:
+    x = ponto[0] + (vetor[0] * t)
+    y = ponto[1] + (vetor[1] * t)
+    z = ponto[2] + (vetor[2] * t)
+    return np.array([x, y, z])
+
+
+def trajetoria():
+    inicio = np.array([0, 25, 0])
+    fim = np.array([50, 50, 50])
+    vetor = fim - inicio
+    segmentos = 10
+    ponto_final = fim
+
+    for s in range(segmentos):
+        p = obtem_ponto_pela_equacao_parametrica(inicio, vetor, s*0.1)
+        ponto_final = p
+        yield Ponto(p[0], p[1], p[2])
+
+    vetor = inicio - fim
+    
+    for s in range(segmentos):
+        p = obtem_ponto_pela_equacao_parametrica(ponto_final, vetor, s*0.1)
+        yield Ponto(p[0], p[1], p[2])
+
+    fim = np.array([0, 0, -50])
+    vetor = fim - inicio
+
+    for s in range(segmentos):
+        p = obtem_ponto_pela_equacao_parametrica(inicio, vetor, s*0.1)
+        ponto_final = p
+        yield Ponto(p[0], p[1], p[2])
+    
+    vetor = inicio - fim
+
+    for s in range(segmentos):
+        p = obtem_ponto_pela_equacao_parametrica(ponto_final, vetor, s*0.1)
+        yield Ponto(p[0], p[1], p[2])
+
 def ponto_aleatorio():
     x = randrange(5, 15)
     y = randrange(5, 15)
@@ -91,7 +138,8 @@ def ponto_aleatorio():
     return Ponto(x, y, z)
 
 
-ani = animation.FuncAnimation(plt.gcf(), plota_normais, curva_parametrica)
+# ani = animation.FuncAnimation(plt.gcf(), plota_normais, curva_parametrica)
+ani = animation.FuncAnimation(plt.gcf(), plota_normais, trajetoria)
 plt.tight_layout()
 
 
--- src/tmp.py
+++ src/tmp.py
@@ -1,92 +1,24 @@
-from weakref import WeakKeyDictionary
-from dataclasses import dataclass
+# importing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import math
 
+v = np.array([4, 1])
+w = 5 * v
+w1 = 0.1 * w
+w2 = 0.2 * w
+w3 = 0.3 * w
+w4 = 0.4 * w
+print(w, w1, w2, w3, w4)
 
-class Cpf:
-    def __init__(self, value=None):
-        self.value = value
+print("w = ", w)
 
-    def __get__(self, instance, cls):
-        return self if instance is None else instance.__dict__[self.value]
-
-    def __set__(self, instance, value):
-        if not self.rule(value):
-            raise TypeError('Cpf inválido')
-        instance.__dict__[self.value] = value
-
-    def __delete__(self, instance):
-        del instance.__dict__[self.value]
-
-    def rule(self, valor):
-        return valor
-
-
-class Cpf2:
-    def __init__(self):
-        self._values = WeakKeyDictionary()
-
-    def __get__(self, instance, instance_type):
-        if instance is None:
-            return self
-        return self._values.get(instance, 0)
-
-    def __set__(self, instance, value):
-        if not self.rule(value):
-            raise TypeError(f'Cpf inválido')
-        self._values[instance] = value
-
-    def rule(self, valor):
-        return valor
-
-
-# class Empregado:
-#     cpf = Cpf
-
-#     def __init__(self, nome, idade, cpf) -> None:
-#         self.nome = nome
-#         self.idade = idade
-#         self.cpf = cpf
-
-# @dataclass()
-class Empregado:
-    nome = str()
-    idade = int()
-    cpf: Cpf2 = Cpf2()
-    rg: Cpf2 = Cpf2()
-    def __init__(self, *args) -> None:
-        self.nome, self.idade, self.cpf, self.rg = args
-    # cpf: Cpf = Cpf()
-    # rg: Cpf = Cpf()
-
-
-emp = Empregado('Ralph', 37, '333', '222')
-emp2 = Empregado('Bruna', 33, '555', '444')
-
-print(emp.nome, emp.cpf, emp.rg)
-print(emp2.nome, emp2.cpf, emp2.rg)
-
-print('mudando cpf', emp.nome)
-emp.cpf = '654'
-print(emp.nome, emp.cpf, emp.rg)
-print(emp2.nome, emp2.cpf, emp2.rg)
-
-print('mudando rg', emp.nome)
-emp.rg = '888'
-print(emp.nome, emp.cpf, emp.rg)
-print(emp2.nome, emp2.cpf, emp2.rg)
-
-print('mudando cpf', emp2.nome)
-emp2.cpf = '654'
-print(emp.nome, emp.cpf, emp.rg)
-print(emp2.nome, emp2.cpf, emp2.rg)
-
-print('mudando rg', emp2.nome)
-emp2.rg = '555'
-print(emp.nome, emp.cpf, emp.rg)
-print(emp2.nome, emp2.cpf, emp2.rg)
-
-# print(emp.cpf, emp.rg)
-# print(emp2.cpf, emp2.rg)
-
-# emp.cpf = Cpf('')
-# print(emp.cpf)
+# Plot w
+origin = [0], [0]
+plt.grid()
+plt.ticklabel_format(style='sci', axis='both', scilimits=(0, 0))
+plt.quiver(*origin, *w, scale=10)
+plt.quiver(*origin, *w1, scale=10)
+plt.quiver(*origin, *w2, scale=10)
+plt.quiver(*origin, *w4, scale=10)
+plt.show()
