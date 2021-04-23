3a73566433b0c2cb94103bc8af18cefb76b000e5
ralphribeiro <ralph_ribeiro@msn.com>
2021-04-23 19:33:00 -0300

    ou não

--- src/factory.py
+++ src/factory.py
@@ -1,4 +1,7 @@
-class Transporte():
+from abc import ABC, abstractmethod
+
+class Transporte(ABC):
+    @abstractmethod
     def fabrica_transporte(self):
         print("fabricando: ")
         ...
@@ -9,10 +12,12 @@ class Transporte():
 
 a = sum([1, 2, 3])
 
-class Tipo():
+class Tipo(ABC):
+    @abstractmethod
     def transporta(self):
         ...
 
+    @abstractmethod
     def no_transporte(self):
         ...
 
@@ -48,6 +53,12 @@ def main(transporte: Transporte):
     transporte.transporta()
 
 
+def print_mro(obj):
+    print(', '.join(str(m) for m in obj.__mro__))
+
+
 if __name__ == "__main__":
+    print_mro(TransporteTrem)
+    print_mro(TransporteCaminhão)    
     main(TransporteTrem())
     main(TransporteCaminhão())
